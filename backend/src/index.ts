720b8e4bacaf1416ca709de3f79f258be5ba80b2
Junwei <lowjunwei1999@gmail.com>
2023-10-01 21:28:34 +0800

    chore: test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 
 const routesList = [venueRouter, eventUnitRouter, bookingsRouter]
 
-console.log("test github actions, gg")
+console.log("test github actions, h")
 
 const createConnection = async () => {
   let retries = 5
